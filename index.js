3ee41bd26484e1a6b65c7ee1007ba1403bf2aed9
Gozde Kalman <gozde.kalman@ibm.com>
2023-06-30 00:23:22 +0300

    first

---
+++ index.js
@@ -0,0 +1,34 @@
+const exp = require("express");
+const bodyParse = require("body-parser");
+const app = exp();
+
+
+app.use(bodyParse.urlencoded({ extended: true }));
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html")
+});
+
+app.post("/", function (req, res) {
+    var num1 = Number(req.body.num1);
+    var num2 = Number(req.body.num2);
+
+    var result = num1 + num2;
+
+    res.send("The result of the calculation is " + result);
+});
+
+
+app.get("/bmicalculator", function (req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html")
+});
+app.post("/bmiCalculator", function (req, res) {
+    var num1 = parseFloat(req.body.weight);
+    var num2 = parseFloat(req.body.height);
+
+    var result = num1 / (num2 * num2);
+
+    res.send("Your BMI is " + result);
+})
+
+app.listen(3000);
